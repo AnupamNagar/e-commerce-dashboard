1eca03896eb91623cbd03d4cc17c5b07cb033188
Anupam Nagar <anupamnagar07@gmail.com>
2023-03-20 23:24:40 +0530

    add script

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.post('/addproduct', async (req, res) => {
     res.send(
         {
             status : 200,
-            message: "Product added succesfully"
+            message: "Product added succesfully"  
 
         }
     );
